1eab695ceafa5c1a4b8215709e0269d093cc481d
kaio <kaiovinicios05@gmail.com>
2025-09-15 18:23:18 -0300

    Finalizando projeto do módulo 21

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+
+    // seção atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton 
@@ -13,6 +29,20 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Seção FAQ, accordion
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
+
+
+
+    function abreOuFechaResposta(elemento) {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode;
+
+        elementoPai.classList.toggle(classe);
+    }
 
     function removeBotaoAtivo() {
         const buttons = document.querySelectorAll('[data-tab-button]');
@@ -30,4 +60,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 })
+
+    function ocultaElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden')
+    }
+
+    function exibeElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden')
+    }
 
